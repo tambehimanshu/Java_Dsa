a2a3c3bbdce0b84b19e000d0fc405c82bbdc502b
tambehimanshu <tambehimanshu18@gmail.com>
2025-02-20 16:38:12 +0530

    basic code with some changes

--- Array/JavaBasic.java
+++ Array/JavaBasic.java
@@ -6,12 +6,18 @@ public class JavaBasic{
 
         int arr[]={1,2,3,4,5};
         int tsr= 1;
+        boolean found = false;
 
      for (int i=0;i<arr.length;i++){
         if(arr[i]==tsr){
-            System.out.println(arr[i]+"target found");
+            System.out.println(arr[i]+" target found at index :"+i);
+            found = true;
         }
      }
+     if(!found){
+        System.out.println("Target not Found");
+     }
+
         
     }
 }
