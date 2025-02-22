2b46cf5df4ec9a9b3636b6fe9066a044fccb7720
tambehimanshu <tambehimanshu18@gmail.com>
2025-02-22 20:32:58 +0530

    largestNo problem

---
+++ Array/largestNo.java
@@ -0,0 +1,16 @@
+public class largestNo {
+    public static void main(String[]args){
+        int largest =Integer.MIN_VALUE;
+
+        int arr []={4,1,3,9,55,3,6,78,98,11};
+         for (int i=0;i<arr.length;i++){
+            if(arr[i]>largest){
+                largest = arr[i];
+
+            }
+            
+    }
+    
+    System.out.println(largest);
+}
+}
