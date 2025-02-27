db0b4b405388b5f1cf1e3a2d2f142c949dbbe040
tambehimanshu <tambehimanshu18@gmail.com>
2025-02-27 10:11:24 +0530

    practice

--- Graphs/Practice.java
+++ Graphs/Practice.java
@@ -3,7 +3,7 @@ import java.util.ArrayList;
 public class Practice {
 
     static class Edge{
-        int src ;
+        int src;
         int dest;
         int wt;
 
@@ -12,38 +12,28 @@ public class Practice {
             this.dest =d;
             this.wt =w;
         }
-
     }
+
     public static void creategraph(ArrayList<Edge> graph[]){
-        for(int i=0;i<graph.length;i++){
+        for(int i =0;i<graph.length;i++){
             graph[i]= new ArrayList<>();
         }
         graph[0].add(new Edge(0, 1, 2));
-        graph[0].add(new Edge(0, 2, 3));
-
         graph[1].add(new Edge(1, 0, 2));
-        graph[1].add(new Edge(1, 3, 1));
-
+        graph[2].add(new Edge(2, 1, 1));
         graph[2].add(new Edge(2, 0, 3));
-        graph[2].add(new Edge(2, 4, 5));
-
-        graph[3].add(new Edge(3, 1, 1));
-        graph[3].add(new Edge(3, 5, 4));
-
-       
     }
-    public static void main(String[]args){
-int v =4;
 
+    public static void main(String []args){
+        int v =4;
 @SuppressWarnings("unchecked")
-ArrayList<Edge> graph[]= new ArrayList[v];
+        ArrayList<Edge> graph[]= new ArrayList[v];
 
-creategraph(graph);
+        creategraph(graph);
 
-for(int i=0;i<graph[2].size();i++){
-    Edge e = graph[2].get(i);
-    System.err.println(e.dest +" "+e.wt);
-}
+     for (int i =0;i< graph[1].size();i++){
+        Edge e = graph[1].get(i);
+        System.err.println(e.dest +" "+e.wt);
+     }
     }
-    
 }
