ec479ffb6f166c29c15a4fd4e586dcb93f8ea9d2
tambehimanshu <tambehimanshu18@gmail.com>
2025-03-14 07:38:07 +0530

    graph printing with all edges

--- Graphs/Practice.java
+++ Graphs/Practice.java
@@ -1,51 +1,43 @@
 import java.util.ArrayList;
 
-
-
-public class Practice {
-
+public class Practice{
     static class Edge{
-int src;
-int dest;
-int wt;
+        int src;
+        int dst;
+        int wt;
 
-public Edge(int s ,int d ,int wt){
-    this.src=s;
-    this.dest=d;
-    this.wt=wt;
-}
-    }
-
-    public static void createGraph(ArrayList<Edge> graph[]){
-        for(int i =0;i<graph.length;i++){
-            graph[i] = new ArrayList<>();
-
-
+        public Edge(int s,int d,int wt){
+            this.src=s;
+            this.dst=d;
+            this.wt=wt;
         }
-        graph[0].add(new Edge(0, 1, 2));
-        graph[0].add(new Edge(0, 2, 3));
-        graph[0].add(new Edge(0, 3, 1));
-        graph[1].add(new Edge(1, 0, 2));
-        graph[1].add(new Edge(1, 3, 1));
-        graph[2].add(new Edge(2, 0, 3));
-        graph[2].add(new Edge(2, 1, 1));
-        graph[3].add(new Edge(3, 1, 1));
-        graph[3].add(new Edge(3, 2, 1));
-        graph[3].add(new Edge(3, 0, 1));
+    }
+    public static void creategraph(ArrayList<Edge> graph[]){
+        for(int i=0;i<graph.length;i++){
+            graph[i]= new ArrayList<>();
+        }
 
+        graph[0].add(new Edge(0, 1, 1));
+        graph[0].add(new Edge(0, 2, 1));
+        graph[0].add(new Edge(0, 3, 0));
 
     }
-   public static void main(String[]args){
-    int v =4;
+    public static void main(String[]args){
+        int v =4;
 
-    @SuppressWarnings("unchecked")
-    ArrayList<Edge> graph[]= new ArrayList[v];
-createGraph(graph);
+       @SuppressWarnings("unchecked")
+       ArrayList<Edge> [] graph= new ArrayList[v];
 
-for(int i=0;i<graph[1].size();i++){
-    Edge e = graph[1].get(i);
-    System.out.println(e.dest+" "+e.wt);
-}
+        creategraph(graph);
 
-   }
+        for (int i = 0; i < graph.length; i++) {
+            System.out.print("Vertex " + i + ": ");
+            for (int j = 0; j < graph[i].size(); j++) {  
+                Edge e = graph[i].get(j);  
+                System.out.print("(" + e.dst + ", " + e.wt + ") ");
+            }
+            System.out.println();
+        }
+
+    }
 }
