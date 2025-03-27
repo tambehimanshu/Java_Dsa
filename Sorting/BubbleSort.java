15b57c2e5a317c85e2edfb5aec64b9b8d9ced3b7
tambehimanshu <tambehimanshu18@gmail.com>
2025-03-27 19:34:36 +0530

    bunnlesort in optimized way

--- Sorting/BubbleSort.java
+++ Sorting/BubbleSort.java
@@ -5,12 +5,17 @@ public class BubbleSort {
         int arr[] = {2,1,3,9,4,8,5};
 
         for (int i=0;i<arr.length-1;i++){
+            boolean swapped =false; //optimized way
             for(int j=0;j<arr.length-i-1;j++){
                 if(arr[j]>arr[j+1]){
                     int temp = arr[j];
                     arr[j]=arr[j+1];
                     arr[j+1]=temp;
+                    swapped =true;
                 }
+            }
+            if(swapped ==false){
+                break;
             }
         }
 
@@ -21,3 +26,5 @@ public class BubbleSort {
     }
     
 }
+// O(n) for best case
+//O(n^2)for worst case.
