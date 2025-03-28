82dfab563202facf27321cde0158055dde5ea97c
tambehimanshu <tambehimanshu18@gmail.com>
2025-03-28 19:15:52 +0530

    updation in nQueens

--- BackTracking/NQueens.java
+++ BackTracking/NQueens.java
@@ -27,6 +27,7 @@ public class NQueens {
         //base case 
         if(row == board.length){
             printBoard(board);
+            count ++;
             return;
         }
         for(int j=0;j<board.length;j++){
@@ -43,11 +44,12 @@ public class NQueens {
         System.out.println("--------------------");
         for(int i =0;i<board.length;i++){
             for(int j=0;j<board.length;j++){
-                System.out.print(board[i][j]);
+                System.out.print(board[i][j]+" ");
             }
             System.out.println();
         }
     }
+    static int count =0;
     public static void main(String[] args) {
         int n =4;
         char board [][]= new char[n][n];
@@ -57,6 +59,8 @@ public class NQueens {
             }
         }
         nQueens(board, 0);
+        System.out.println("total ways to solve nQueens : "+count);
     }
     
 }
+//tc O(n!)
