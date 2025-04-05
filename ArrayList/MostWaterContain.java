e5d0157551870daf28eca86b063ad1811a642257
tambehimanshu <tambehimanshu18@gmail.com>
2025-04-05 18:01:29 +0530

    optimized way of finding max stored water

--- ArrayList/MostWaterContain.java
+++ ArrayList/MostWaterContain.java
@@ -3,18 +3,43 @@ package ArrayList;
 import java.util.ArrayList;
 
 public class MostWaterContain {
-    public static int  storeWater(ArrayList<Integer> height){
-        int max= 0;
-        for(int i =0;i<height.size();i++){
-            for(int j =i+1;j<height.size();j++){
-                int h = Math.min(height.get(i), height.get(j));
-                int w = j-i;
-                int currWater = h*w;
-                max =Math.max(max, currWater);
+    // public static int  storeWater(ArrayList<Integer> height){
+    //     int max= 0;
+    //     for(int i =0;i<height.size();i++){
+    //         for(int j =i+1;j<height.size();j++){
+    //             int h = Math.min(height.get(i), height.get(j));
+    //             int w = j-i;
+    //             int currWater = h*w;
+    //             max =Math.max(max, currWater);
+    //         }
+    //     }
+    //     return max;
+
+    // }
+
+
+
+    //BY two pointer approch  
+    public static int storeWater(ArrayList<Integer> height)
+    {
+        int maxwater =0;
+        int left = 0;
+        int right = height.size()-1;
+        while (left<right) {
+            int ht =Math.min(height.get(left), height.get(right));
+
+            int wt = right - left;
+            int current = ht *wt;
+            maxwater = Math.max(maxwater, current);
+            
+            if(height.get(left)< height.get(right)){
+                left ++;
+            }
+            else{
+                right --;
             }
         }
-        return max;
-
+        return maxwater;
     }
     public static void main(String[] args) {
         ArrayList<Integer> height = new ArrayList<>();
@@ -33,4 +58,6 @@ public class MostWaterContain {
     }
     
 }
-//tc O(n^2);
+//tc O(n^2) for brute force 
+//tc O(n) for optimized 2pointer approch 
+
