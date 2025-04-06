78b3e15f1b94491b2c18bc0357570fd58d01b02b
tambehimanshu <tambehimanshu18@gmail.com>
2025-04-06 11:04:29 +0530

    optimized way of pair sum-1

--- ArrayList/PairSum.java
+++ ArrayList/PairSum.java
@@ -2,15 +2,39 @@ package ArrayList;
 
 import java.util.ArrayList;
 
+
 public class PairSum {
-    public static boolean sum(ArrayList<Integer> list, int target){
-        for(int i =0;i<list.size();i++){
-            for (int j=i+1;j<list.size();j++){
-                if(target == list.get(i)+list.get(j)){
-                    return true;
-                }else{
-                    return false;
-                }
+    // public static boolean sum(ArrayList<Integer> list, int target){
+    //     for(int i =0;i<list.size();i++){
+    //         for (int j=i+1;j<list.size();j++){
+    //             if(target == list.get(i)+list.get(j)){
+    //                 return true;
+    //             }else{
+    //                 return false;
+    //             }
+    //         }
+    //     }
+    //     return false;
+    // }
+
+    public static boolean sum(ArrayList<Integer> list , int target){
+        int leftP= 0;
+        int rightP = list.size()-1;
+
+        while (leftP < rightP) {
+            
+        
+            //case1
+            if(list.get(leftP)+list.get(rightP)== target){
+                return true;
+            } 
+            //case2
+            if (list.get(leftP)+list.get(rightP)> target){
+                rightP--;
+            }
+            //case 3
+            else{
+                leftP++;
             }
         }
         return false;
@@ -29,3 +53,4 @@ public class PairSum {
     
 }
 //tc O(n^2) brute force approch 
+//tc O(n) 2 pointer optimized way
