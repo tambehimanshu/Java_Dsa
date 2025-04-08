de163601133ddee167536e4b2b4735de5e5e9619
tambehimanshu <tambehimanshu18@gmail.com>
2025-04-08 19:20:11 +0530

    ll with using java collection framework

---
+++ LinkedList/LLwithJCF.java
@@ -0,0 +1,20 @@
+package LinkedList;
+import java.util.*;
+import java.util.LinkedList;
+
+
+public class LLwithJCF {
+
+    public static void main(String[] args) {
+        LinkedList<Integer> ll = new LinkedList<>();
+
+        ll.addLast(1);
+        ll.addLast(2);
+        ll.addFirst(0);
+        System.out.println(ll);
+        ll.removeLast();
+        ll.removeFirst();
+        ll.remove(0);
+        System.out.println(ll);
+    }
+}
