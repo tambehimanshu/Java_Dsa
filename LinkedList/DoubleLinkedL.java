7122f6bc66adc299208b0bc5944433b520742c7a
tambehimanshu <tambehimanshu18@gmail.com>
2025-04-09 19:17:48 +0530

    remove first and last in dll

--- LinkedList/DoubleLinkedL.java
+++ LinkedList/DoubleLinkedL.java
@@ -49,12 +49,64 @@ public void addLast(int data){
     newNode.prev = tail;
     tail = newNode;
 }
+public int removefirst(){
+    size--;
+    if(head == null){
+        System.out.println("ddl is empty");
+        return Integer.MIN_VALUE;
+
+    }
+
+    //for single node in dddl
+    if(size == 1){
+        int val = head.data;
+        head = null;
+        tail = null;
+        size=0;
+
+        return val;
+    }
+    int val = head.data;
+    head = head.next; //head move to next Node
+    head.prev = null;
+    return val;
+
+
+}
+public int removelast(){
+    
+    if(head == null){
+        System.out.println("ddl is mepty");
+        return Integer.MAX_VALUE;
+
+    }
+    if(size == 1){
+        int val = tail.data;
+        tail= null;
+        head = null;
+        size =0;
+        return val;
+    }
+    Node prev = head ;
+    for(int i =0; i<size -2;i++){
+        prev = prev.next;
+    }
+
+    int val = prev.next.data;
+    prev.next = null;
+    tail = prev;
+    size--;
+    return val;
+
+}
     public static void main(String[] args) {
     DoubleLinkedL dll = new DoubleLinkedL();
     dll.addFirst(3);
     dll.addFirst(2);
     dll.addFirst(1);
     dll.addLast(0);
+    dll.print();
+    dll.removelast();
     dll.print();
     System.out.println(dll.size);
     }
