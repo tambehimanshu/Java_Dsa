de774a9a8ba3ce32f38998d376311ca39c1ec5fd
tambehimanshu <tambehimanshu18@gmail.com>
2025-04-10 17:34:33 +0530

    duplicate removal from the ll

---
+++ LinkedList/RemovelOfDuplicate.java
@@ -0,0 +1,60 @@
+package LinkedList;
+
+import java.util.HashSet;
+
+public class RemovelOfDuplicate {
+
+    static class Node{
+        int data;
+        Node next;
+
+        public Node(int data){
+            this.data= data;
+            this.next = null;
+        }
+    }
+    public static Node removedup(Node head){
+        if(head == null){
+            return null;
+        }
+
+        HashSet<Integer> seen = new HashSet<>();
+        Node curr = head;
+        Node prev = null;
+
+        while(curr != null){
+            if(seen.contains(curr.data)){
+                prev.next = curr.next;
+            }else {
+                seen.add(curr.data);
+                prev = curr;
+            }
+            curr = curr.next;
+        }
+        return head;
+    }
+     public static void print(Node head){
+        Node temp = head;
+
+        while(temp != null){
+            System.out.print(temp.data+"->");
+            temp= temp.next;
+        }
+        System.out.println("Null");
+     }
+    public static void main(String[] args) {
+        Node head = new Node(1);
+        head.next = new Node(2);
+        head.next.next = new Node(3);
+        head.next.next.next= new Node(4);
+        head.next.next.next .next = new Node(4);
+
+        print(head);
+
+        head = removedup(head);
+
+        print(head);
+    }
+    
+} //tc O(n)
+//sp O(n)
