3db3da0b3a803322981a2c320e84c016c59b761c
tambehimanshu <tambehimanshu18@gmail.com>
2025-04-11 15:46:32 +0530

    optimized way of reversing array

--- Array/ReverseArray.java
+++ Array/ReverseArray.java
@@ -15,9 +15,22 @@ public class ReverseArray {
             
          }
     }
+
+    public static void rev(int arr[]){
+         
+        int n = arr.length;
+        for(int i =0;i<n/2;i++){
+            int temp = arr[i];
+            arr[i]= arr[n-1-i];
+            arr[n-1-i] = temp;
+
+        }
+    }
     public static void main(String[] args) {
         int arr[]={1,2,3,4,5,6,7,8,9};
-        reverse(arr);
+        //reverse(arr);
+
+        rev(arr);
 
         for(int i=0;i<arr.length;i++){
             System.out.print(arr[i]+" ");
@@ -25,4 +38,5 @@ public class ReverseArray {
         
     }
     
-}
+}//tc O(n)
+//sc O(1)
