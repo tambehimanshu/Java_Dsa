1b2eac723286049b7f33939a7408b71275fe3dc6
tambehimanshu <tambehimanshu18@gmail.com>
2025-04-12 16:28:53 +0530

    reverse strin with stack

---
+++ Stack/ReverseString.java
@@ -0,0 +1,30 @@
+package Stack;
+
+import java.util.Stack;
+
+public class ReverseString {
+    public static String revString(String str){
+        Stack<Character> s = new Stack<>();
+        int idx =0;
+        while(idx < str.length()){
+            s.push(str.charAt(idx));
+            idx++;
+        }
+
+        StringBuilder result = new StringBuilder("");
+        while (!s.isEmpty()) {
+            char curr = s.pop();
+            result.append(curr);
+            
+        }
+        return result.toString();
+
+
+    }
+    public static void main(String[] args) {
+        String str = "fog";
+        String result = revString(str);
+        System.out.println(result);
+    }
+    
+}
