e82839f7dc9fe878b89579551915a6703818d8b3
tambehimanshu <tambehimanshu18@gmail.com>
2025-04-12 21:18:56 +0530

    stock span problem

---
+++ Stack/StockSpan.java
@@ -0,0 +1,38 @@
+package Stack;
+
+import java.util.Stack;
+
+public class StockSpan {
+    public static void stockss(int stocks[],int span[]){
+        Stack<Integer> s = new Stack<>();
+        span[0]=1;
+        s.push(0);for(int i =1;i<stocks.length;i++){
+            int currPrice = stocks[i];
+            while (!s.isEmpty() && currPrice >stocks[s.peek()]) {
+                //remove small size elements
+
+                s.pop();
+                
+            }
+            if(s.isEmpty()){
+                span[i]= i+1;
+            }
+            else{
+                int prevHigh = s.peek();
+                span[i] = i-prevHigh;
+            }
+            s.push(i);
+        }
+    }
+    public static void main(String[] args) {
+        int stocks[]= {100,80,60,70,50,85,100};
+        int span[]= new int [stocks.length];
+        stockss(stocks,span);
+
+        for(int i =0;i<span.length;i++){
+            System.out.println(span[i]);
+        }
+    }
+    
+}
+
