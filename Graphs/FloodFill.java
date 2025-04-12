4e12a30a5c4a408a5dc9ac230796c40e49f6f5a9
tambehimanshu <tambehimanshu18@gmail.com>
2025-04-12 10:37:56 +0530

    flood fill

--- Graphs/FloodFill.java
+++ Graphs/FloodFill.java
@@ -1,12 +1,13 @@
 public class FloodFill {//O(m*n)
 
 
-public void helper(int [][]image,int sr,int sc,int color,boolean vis[][],int orgcolor){
-    if(sr<0 || sc <0 || sr >= image.length|| sc> image[0].length|| vis[sr][sc] || image[sr][sc]!=orgcolor){
+public  static void helper(int [][]image,int sr,int sc,int color,boolean vis[][],int orgcolor){
+    if(sr<0 || sc <0 || sr >= image.length|| sc>= image[0].length|| vis[sr][sc] || image[sr][sc]!=orgcolor){
         return;
 
     }
-    
+    vis[sr][sc]=true;
+    image[sr][sc] = color;
     // left
     helper(image, sr, sc-1, color, vis, orgcolor);
 
@@ -20,13 +21,30 @@ public void helper(int [][]image,int sr,int sc,int color,boolean vis[][],int org
     helper(image, sr+1, sc, color, vis, orgcolor);
 }
 
-    public int [][] floodfil(int [][]image,int sr,int sc,int color){ // sr=starting row sc = col
+    public static int [][] floodfil(int [][]image,int sr,int sc,int color){ // sr=starting row sc = col
         boolean vis[][]=new boolean[image.length][image[0].length];
         helper(image,sr,sc,color,vis,image[sr][sc]);
 return image;    
     }
     public static void main(String[]args){
-        
+        int  [][] image = {{1,1,1},
+                         {1,1,0},
+                        {1,0,1}
+                    };
+
+                    int sr = 1;
+                    int sc = 1;
+                   int  newCol = 2;
+
+                    int [][]res = floodfil(image,sr,sc,newCol);
+
+                    
+        for (int[] row : res) {
+            for (int pixel : row) {
+                System.out.print(pixel + " ");
+            }
+            System.out.println();
+        }
     
     }
 }
