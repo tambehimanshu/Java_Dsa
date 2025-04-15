65d512f0249c44d9eb6286f70dabc7ecf35da7a3
tambehimanshu <tambehimanshu18@gmail.com>
2025-04-15 22:37:27 +0530

    circular q implementation

--- Queue/QueueUsingArray.java
+++ Queue/QueueUsingArray.java
@@ -1,29 +1,39 @@
 package Queue;
 
 
-public class Queue {
+public class QueueUsingArray {
     static class Q{
     static int arr[];
     static int size;
     static int rear;
+    static int front;
     
     Q(int n){
         arr= new int[n];
         size = n;
         rear = -1;
+        front = -1;
     }
 
     public static boolean isEmpty(){
-        return rear==-1;
+        return rear==-1 && front ==-1;  
+    }
+    public static boolean isFull(){
+        return (rear+1) %size == front;
     }
 
     public static void add(int data){
-        if(rear == size-1) {
+        if(isFull()) {
             System.out.println("Q is full");
             return;
         }
 
-        rear = rear+1;
+        if (front ==-1) {
+            front = 0;
+            
+        }
+
+        rear = (rear+1)%size;
         arr[rear]= data; 
     }
 public static int remove(){
@@ -31,36 +41,44 @@ public static int remove(){
         System.out.println("q is empty");
         return -1;
     }
-
-    int front  =arr[0];
-    for(int i =0;i<rear;i++){
-        arr[i]= arr[i+1];
+int res = arr[ front];
+   
+    if(front == rear){
+        front =-1;
+        rear =-1;
+    }else {
+        front  =(front+1)%size;
     }
-    rear--;
-    return front;
+
+    return res;
 }
 public static int peek(){
     if(isEmpty()){
         return -1;
     }
-    return arr[0];
+    return arr[front];
 }
 
     }
     
     
     public static void main(String[] args) {
-        Q q = new Q(5);
+        Q q = new Q(3);
         Q.add(1);
         Q.add(2);
-        Q.add(33);
-        Q.add(30);
-
+        Q.add(3);
+        System.out.println(q.remove());
+        q.add(4);
+    
+        System.out.println(q.remove());
+        q.add(5);
+        
         while (!q.isEmpty()) {
-            System.out.print(q.peek() +" ");
+            System.out.println(q.peek()+" ");
             q.remove();
             
         }
+
 
         
     }
