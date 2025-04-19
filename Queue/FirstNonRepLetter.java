044e3d61197545d39e3fb1e015c1b3fe381ac27f
tambehimanshu <tambehimanshu18@gmail.com>
2025-04-19 22:04:29 +0530

    first Non Repeating Letter with using queue

---
+++ Queue/FirstNonRepLetter.java
@@ -0,0 +1,40 @@
+package Queue;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class FirstNonRepLetter {
+    public static void findNonRep(String s){
+        int freq [ ] = new int[26];
+        Queue<Character> q = new LinkedList<>();
+
+        for(int i =0;i<s.length();i++){
+            char ch = s.charAt(i);
+            q.add(ch);
+            freq[ch-'a']++;
+
+            while (!q.isEmpty() && freq[q.peek()-'a']> 1) {
+                q.remove();
+                
+            }
+            if (q.isEmpty()) {
+                System.out.print(-1+" ");
+                
+            }else{
+                System.out.print(q.peek()+" ");
+            }
+
+
+
+        }
+        System.out.println();
+    }
+    public static void main(String[] args) {
+        String s  ="aabccxb"; 
+        findNonRep(s);
+    }
+
+}
+
+//tc O(n)
+//sc O(n)
