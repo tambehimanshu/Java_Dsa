779f5ebc634f09e3eeeb1197bebd99f85ec6876a
tambehimanshu <tambehimanshu18@gmail.com>
2025-04-29 12:04:08 +0530

    insertion sort practice

--- Sorting/Practice.java
+++ Sorting/Practice.java
@@ -3,28 +3,21 @@ package Sorting;
 public class Practice {
     public static void main(String[] args) {
         
-    
+        //insertion sort (where we make to bunch of sorted and unsorted)
+        int arr[] = {9,2,4,1,6,8};
 
-   //selection sort 
+        for(int i =1;i<arr.length;i++){
+            int current = i;
+            int j =i-1;
+            while(j>=0 && current <arr[j]){
+                arr[j+1]= arr[j];
+                j--;
 
-   int arr[]={3,9,4,1,7,2};
-
-   for(int i =0;i<arr.length;i++){
-    int smallest = i;
-    for(int j =i+1;j<arr.length;j++){
-        if(arr[smallest]> arr[j]){
-            smallest = j;
+            }
+            arr[j+1] = current;
         }
-    }
-    // if smallest get updated with j means there is a eleemnt smaller than i so swap 
-    // so now smallest = j and swap with i
-    // and if smallest = arr[i] is the smallest then the swap will be done with smallest of i and arr of i itself so now change will be seen
-    int temp = arr[smallest];
-    arr[smallest] = arr[i];
-    arr[i] = temp;
-   }
-   for(int i =0;i<arr.length;i++){
-    System.out.println(arr[i] +" ");
-   }
+        for(int i =0;i<arr.length;i++){
+            System.out.println(arr[i]+" ");
+        }
 }
 }
