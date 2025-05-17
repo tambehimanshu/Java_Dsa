9ef9c27f04083cf450ab2f26748ca8d234b51be0
tambehimanshu <tambehimanshu18@gmail.com>
2025-05-17 10:33:30 +0530

    Maximum Path in tree

--- Tree/MaximumPathSum.java
+++ Tree/MaximumPathSum.java
@@ -32,14 +32,34 @@ public class MaximumPathSum {
         }
     }
    
+public static int findMax(Node root){ //tc O(n) sc O(n)
 
+    int max[] =new int[1];
+    max[0]=Integer.MIN_VALUE;
+    maxdownpath(root,max);
+    return max[0];
+
+}
+public static int maxdownpath(Node root,int[] max){
+    if(root == null){
+        return 0;
+    }
+
+    int left = maxdownpath(root.left , max);
+    int right = maxdownpath(root.right, max);
+
+    max[0] = Math.max(max[0], left+right+root.data);
+
+    return root.data+ Math.max(left, right);
+}
     
 
     public static void main(String[] args) {
-        int []nodes = {1,2,3,4,-1,-1,5,-1,-1,6,-1,-1,5};
+        int []nodes = {-10,9,-1,-1,20,15,-1,-1,7,-1,-1};
         BinaryTree tree = new BinaryTree();
          Node root = tree.buildTree(nodes);
       
+         System.out.println(findMax(root));
     }
     
 }
