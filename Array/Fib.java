c792d933b65759d307fd285e62a1ebc10afe3b45
tambehimanshu <tambehimanshu18@gmail.com>
2025-06-19 23:08:11 +0530

    fib code

---
+++ Array/Fib.java
@@ -0,0 +1,14 @@
+public class Fib {
+
+    public static int fibb(int n){
+        if(n <= 1) return n ;
+         return fibb(n-1)+ fibb(n-2);
+   
+    }
+
+    public static void main(String[] args) {
+        int n = 5;
+        System.out.println(fibb(n));
+    }
+    
+}
