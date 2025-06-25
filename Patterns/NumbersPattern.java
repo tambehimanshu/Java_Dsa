a4a2a2526e22e0a78bcfed5b9085391064f3fdbf
tambehimanshu <tambehimanshu18@gmail.com>
2025-06-25 18:59:07 +0530

    floyds triangle pattern

---
+++ Patterns/NumbersPattern.java
@@ -0,0 +1,13 @@
+public class NumbersPattern {
+    public static void main(String[] args) {
+      int counter =1;
+      int row =5;
+        for(int i =1 ;i<=row;i++){
+            for(int j =1;j<=i;j++){
+                System.out.print(counter +" ");
+                counter++;
+            }
+            System.out.println();
+        }
+    }
+}
