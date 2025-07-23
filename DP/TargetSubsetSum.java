e67f2f59ff821b3bf7440f23da22dadbb0fe6012
tambehimanshu <tambehimanshu18@gmail.com>
2025-07-23 22:08:07 +0530

    memoization code of target SubSet Sum

--- DP/TargetSubsetSum.java
+++ DP/TargetSubsetSum.java
@@ -1,18 +1,41 @@
+import java.util.Arrays;
+
 public class TargetSubsetSum {
-    public static boolean isSubset(int arr[],int target,int n ){
+    public static boolean isSubsetR(int arr[],int target,int n ){
         if(n == 0 ) return false;
         if(target == 0) return true;
 
         if(arr[n-1] <= target){
-            return isSubset(arr, target-arr[n-1], n-1)|| isSubset(arr, target, n-1);
+            return isSubsetR(arr, target-arr[n-1], n-1)|| isSubsetR(arr, target, n-1);
         }
-        return isSubset(arr, target, n-1);
+        return isSubsetR(arr, target, n-1);
     }
+     public static boolean isSubsetM(int arr[],int target,int n,Boolean dp[][] ){
+        if(n == 0 ) return false;
+        if(target == 0) return true;
+
+        if(dp[n][target] != null){
+            return dp[n][target];
+        }
+
+        if(arr[n-1] <= target){
+            dp[n][target] =isSubsetM(arr, target-arr[n-1], n-1,dp)|| isSubsetM(arr, target, n-1,dp);
+        }
+        else{
+            dp[n][target] = isSubsetM(arr, target, n-1, dp);
+        }
+        return dp[n][target];
+    }
+
     public static void main(String[] args) {
         int arr[]={1,2,3,4};
         int target = 5;
         int n = arr.length;
+        Boolean dp[][]= new Boolean[n+1][target+1];
+        for(Boolean row[] :dp ){
+            Arrays.fill(row, null);
+        }
 
-        System.out.println(isSubset(arr,target,n));
+        System.out.println(isSubsetM(arr,target,n,dp));
     }
 }
