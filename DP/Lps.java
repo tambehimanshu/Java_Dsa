391cd4e5a1bf405b1f8d14a5871c572d5c8677e2
tambehimanshu <tambehimanshu18@gmail.com>
2025-07-27 23:17:42 +0530

    optimized code of lps

---
+++ DP/Lps.java
@@ -0,0 +1,31 @@
+package DP;
+
+public class Lps {//longest palindromic subsequence tc O(N^2) sc O(n)
+  public static int findLps(String text1,String text2){
+         int n = text1.length();
+        int m = text2.length();
+
+        int prev []= new int [m+1];
+        int curr []= new int[m+1];
+        for(int i =1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(text1.charAt(i-1)==text2.charAt(j-1)){
+                    curr[j] =1+prev[j-1];
+                }
+                else{
+                    curr[j]= Math.max(prev[j],curr[j-1]);
+                }
+            }
+            prev = (int[])(curr.clone());
+        }
+        return prev[m];
+    
+    }
+    public static void main(String[] args) {
+        String s1 = "abbabbcaa";
+        String s2 = new StringBuilder(s1).reverse().toString();
+
+        System.out.println(findLps(s1,s2));
+    }
+    
+}
