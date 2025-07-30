9070c25ed925c7588e09d8654b3eea922d385519
tambehimanshu <tambehimanshu18@gmail.com>
2025-07-31 00:04:50 +0530

    optimized code of stock buy n sell 4

--- DP/Stock/Stock_V.java
+++ DP/Stock/Stock_V.java
@@ -1,6 +1,8 @@
 package DP.Stock;
+//memo and tabu code same as the prev stock codes
 
 public class Stock_V {
+
     public static int f(int ind , int buy ,int prices[],int fee,int n){
         if(ind ==n )return 0;
 
@@ -16,10 +18,29 @@ public class Stock_V {
 
         return  f(0,1 , prices ,fee,n);
     }
+    
+    public static int funOptimized(int prices[] ,int fee){ // tc O(1) sc O(n)
+        int n = prices.length;
+        int aheadBuy =0, aheadNotBuy =0;
+        for(int ind =n-1;ind>=0;ind--){
+            int currBuy = Math.max(-prices[ind]+aheadNotBuy,aheadBuy);
+
+            int currNotBuy = Math.max(prices[ind] -fee + aheadBuy, aheadNotBuy);
+
+
+            aheadBuy = currBuy;
+            aheadNotBuy = currNotBuy;
+        }
+        return aheadBuy;
+
+    }
+    
     public static void main(String[] args) {
-        int prices[]= {1,2,3,6,1,4};
+        int prices[]= {1,2,3,6,1,5};
         int fee =2;
 
-        System.out.println(funRec(prices,fee));
+       // System.out.println(funRec(prices,fee));
+       System.out.println(funOptimized(prices, fee));
+
     }
 }
