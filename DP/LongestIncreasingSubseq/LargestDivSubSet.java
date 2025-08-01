4cf6aa3fd0e650ece1dcb642520b710eca47fb36
tambehimanshu <tambehimanshu18@gmail.com>
2025-08-01 12:29:37 +0530

    length of largest divisible subset

--- DP/LongestIncreasingSubseq/LargestDivSubSet.java
+++ DP/LongestIncreasingSubseq/LargestDivSubSet.java
@@ -37,6 +37,28 @@ public class LargestDivSubSet { // tcc o(n2) scO(n)
         
 
     }
+
+    //  only for printing length
+    public static int largestDivisibleSubsetLength(int[] nums) {
+        int n = nums.length;
+        if (n == 0) return 0;
+
+        Arrays.sort(nums);  // Sorting is essential
+        int[] dp = new int[n];
+        Arrays.fill(dp, 1);
+        int maxLen = 1;
+
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (nums[i] % nums[j] == 0) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+            maxLen = Math.max(maxLen, dp[i]);
+        }
+
+        return maxLen;
+    }
     public static void main(String[] args) {
         int arr[] = {1,16,7,4,8};
 
@@ -46,6 +68,8 @@ public class LargestDivSubSet { // tcc o(n2) scO(n)
         for(int num: result){
             System.out.print(num+" ");
         }
+
+        System.out.println("length is : "+largestDivisibleSubsetLength(arr));
     }
     
 }
