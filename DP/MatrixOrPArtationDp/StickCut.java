142379a2f35a248a2f622c640b587c7fdd6fd079
tambehimanshu <tambehimanshu18@gmail.com>
2025-08-02 13:59:51 +0530

    tabulation of cut stick

--- DP/MatrixOrPArtationDp/StickCut.java
+++ DP/MatrixOrPArtationDp/StickCut.java
@@ -32,10 +32,37 @@ public class StickCut {
         }
         return minCost;
         }
+    public static int minCostToCutStickTabu(int[] cuts, int n) { // tc O(n^3) sc O(n^2)
+        
+        int m = cuts.length;
+        int newCuts[] = new int[m + 2];  // +2 coz adding 0 to the start and n to the end
+        newCuts[0] = 0; 
+        newCuts[m + 1] = n;    
+        for (int i = 1; i <= m; i++) {
+            newCuts[i] = cuts[i - 1]; 
+        }
+        java.util.Arrays.sort(newCuts);
+
+        int dp[][] = new int[m + 2][m + 2];
+
+        for (int len = 2; len <= m + 1; len++) {
+        for (int i = 0; i + len <= m + 1; i++) {
+            int j = i + len; // last ind to cal cut
+            dp[i][j] = Integer.MAX_VALUE;
+            for (int k = i + 1; k < j; k++) {
+                int cost = newCuts[j] - newCuts[i] + dp[i][k] + dp[k][j];
+                dp[i][j] = Math.min(dp[i][j], cost);
+            }
+        }
+    }
+    return dp[0][m+1];
+    }
     public static void main(String[] args) {
         int[] arr = {1,3, 4, 5};
         int n = 7;
 
-        System.out.println("Minimum cost to cut the stick: " + minCostToCutStick(arr, n));
+       /// System.out.println("Minimum cost to cut the stick: " + minCostToCutStick(arr, n));
+
+       System.out.println("Minimum cost to cut the stick: " + minCostToCutStickTabu(arr, n));
     }
 }
