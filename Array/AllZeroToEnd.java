7806169e84e884d06c60f278d6c8add8967f3d94
tambehimanshu <tambehimanshu18@gmail.com>
2025-09-04 10:46:09 +0530

    revision and updation

--- Array/AllZeroToEnd.java
+++ Array/AllZeroToEnd.java
@@ -13,10 +13,25 @@ public class AllZeroToEnd {
             }
         }
     }
+
+    // OR
+     public static void allZeros(int []arr){
+        int count =0;
+
+        for(int num :arr){
+            if(num !=0){
+                arr[count++]=num;
+            }
+        }
+        while(count <arr.length){
+            arr[count++]=0;
+        }
+     }
     public static void main(String[] args) {
         int arr [] ={20,10,0,1,0,4,0,3};
         
         allZero(arr);
+        //allZeros(arr);
         for (int i =0;i<arr.length;i++){
             System.out.print(arr[i]+" ");
         }
